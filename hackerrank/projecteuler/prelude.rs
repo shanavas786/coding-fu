830e6465f3710d1c26fe90dc2b8c29077d73a69c
Shanavas M <shanavas.m2@gmail.com>
2019-01-24 22:50:33 +0530

    project euler 1

---
+++ hackerrank/projecteuler/prelude.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+macro_rules! make_reader {
+    ($fname:ident, $type:ty) => {
+        pub fn $fname() -> $type {
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+            let num: $type = input.trim().parse().expect("Please type a number");
+            num
+        }
+    };
+}
+
+make_reader! {read_i32, i32}
--- hackerrank/projecteuler/prob001.rs
+++ hackerrank/projecteuler/prob001.rs
@@ -1,1 +1,22 @@
+mod prelude;
 
+use prelude::read_i32;
+
+// Find the sum of all the multiples of 3 or 5 below N.
+
+fn sum_of_multiples(n: i32) -> i32 {
+    (1..n).rfold(0, |acc, x: i32| {
+        if x % 3 == 0 || x % 5 == 0 {
+            acc + x
+        } else {
+            acc
+        }
+    })
+}
+
+fn main() {
+    let num = read_i32();
+    for _ in 0..num {
+        println!("{}", sum_of_multiples(read_i32()));
+    }
+}
