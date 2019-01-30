c0f4e6804623df83f7d72cfe45215a7b020f75bf
Shanavas M <shanavas.m2@gmail.com>
2019-01-30 20:57:44 +0530

    Use a simpler algorithm

--- hackerrank/projecteuler/problem3/src/main.rs
+++ hackerrank/projecteuler/problem3/src/main.rs
@@ -1,43 +1,58 @@
-use prelude::{Prime, read_i64};
+use prelude::read_i64;
 
 /// What is the largest prime factor of a given number
-fn largest_prime_factor(prime: &mut Prime, num: i64) -> i64 {
-    let mut max = num;
-
-    let mut max_prime = 1;
-    let mut i = 1;
-
-    while i < max {
-        if num % i == 0 {
-            let factor = num / i;
-            if prime.is_prime(factor) {
-                return factor;
-            }
-            if prime.is_prime(i) {
-                max_prime = i;
-            }
-            max = factor;
-        }
-        i += 1;
+fn largest_prime_factor(mut num: i64) -> i64 {
+    if num < 0 {
+        return 1;
+    } else if num < 2 {
+        return num;
     }
 
-    max_prime
+    while num % 2 == 0 {
+        num /= 2;
+    }
+
+    if num == 1 {
+        return 2;
+    }
+
+    // num must be odd now
+    let mut factor = 3;
+    while factor <= (num as f64).sqrt() as i64 {
+        if num == factor {
+            break;
+        } else if num % factor == 0 {
+            num /= factor;
+            factor = 3;
+        } else {
+            factor += 2;
+        }
+    }
+
+    if num > 2 {
+        num
+    } else {
+        factor
+    }
 }
 
 fn main() {
-    let mut prime = Prime::new();
     let num = read_i64();
     for _ in 0..num {
-        println!("{}", largest_prime_factor(&mut prime, read_i64()));
+        println!("{}", largest_prime_factor(read_i64()));
     }
 }
 
 #[test]
 fn test_max_prime() {
-    let mut prime = Prime::new();
-
-    assert_eq!(largest_prime_factor(&mut prime, 10), 5);
-    assert_eq!(largest_prime_factor(&mut prime, 17), 17);
-    assert_eq!(largest_prime_factor(&mut prime, 13195), 29);
-    assert_eq!(largest_prime_factor(&mut prime, 31*6), 31);
+    assert_eq!(largest_prime_factor(10), 5);
+    assert_eq!(largest_prime_factor(-1), 1);
+    assert_eq!(largest_prime_factor(0), 0);
+    assert_eq!(largest_prime_factor(1), 1);
+    assert_eq!(largest_prime_factor(9), 3);
+    assert_eq!(largest_prime_factor(2), 2);
+    assert_eq!(largest_prime_factor(3), 3);
+    assert_eq!(largest_prime_factor(17), 17);
+    assert_eq!(largest_prime_factor(13195), 29);
+    assert_eq!(largest_prime_factor( 31*6), 31);
 }
