1c342d396777e70bb22926e371180e35e2d26624
Shanavas M <shanavas.m2@gmail.com>
2025-06-27 12:12:05 +0530

    [aoc24] day-12 part 2

--- adventofcode/2024/day-12.c
+++ adventofcode/2024/day-12.c
@@ -7,15 +7,16 @@
 #define GRID_SIZE (256 * 256)
 
 void trace(char **maze, int *marker, int rows, int cols, int row, int col,
-           int *area, int *perim) {
+           int *area, int *perim, int *sides) {
   char sym = maze[row][col];
   int idx = row * rows + col;
   stack_init();
   stack_push(idx);
-  int left, right, top, bottom, r, c;
+  int left, right, top, bottom, r, c, adj;
 
   *area = 0;
   *perim = 0;
+  *sides = 0;
   while (stack_size()) {
     idx = stack_pop();
 
@@ -81,6 +82,48 @@ void trace(char **maze, int *marker, int rows, int cols, int row, int col,
     } else {
       *perim += 1;
     }
+
+    // check for corners
+    // edge at left
+    if (c == 0 || maze[r][c - 1] != sym) {
+      if (r == 0 || maze[r - 1][c] != sym) {
+        // top edge
+        // corner at top left
+        *sides += 1;
+      } else if (c > 0 && maze[r - 1][c - 1] == sym) {
+        *sides += 1;
+      }
+    }
+
+    // top edge
+    if (r == 0 || maze[r - 1][c] != sym) {
+      // corner at top right
+      if (c == cols - 1 || maze[r][c + 1] != sym) {
+        *sides += 1;
+      } else if (r > 0 && maze[r - 1][c + 1] == sym) {
+        *sides += 1;
+      }
+    }
+
+    // right edge
+    if (c == cols - 1 || maze[r][c + 1] != sym) {
+      if (r == rows - 1 || maze[r + 1][c] != sym) {
+        // corner at bottom right
+        *sides += 1;
+      } else if (c < cols - 1 && maze[r + 1][c + 1] == sym) {
+        *sides += 1;
+      }
+    }
+
+    // bottom edge
+    if (r == rows - 1 || maze[r + 1][c] != sym) {
+      if (c == 0 || maze[r][c - 1] != sym) {
+        // corner at bottom left
+        *sides += 1;
+      } else if (r < rows - 1 && maze[r + 1][c - 1] == sym) {
+        *sides += 1;
+      }
+    }
   }
 }
 
@@ -105,7 +148,9 @@ int main(int argc, char *argv[]) {
   int *marker = calloc(rows * cols, sizeof(int));
   int area;
   int perimeter;
-  long sum = 0;
+  int sides;
+  long sum1 = 0;
+  long sum2 = 0;
   int idx;
 
   for (int row = 0; row < rows; row++) {
@@ -114,13 +159,16 @@ int main(int argc, char *argv[]) {
       if (marker[idx] == MARKED) {
         continue;
       }
-      trace(maze, marker, rows, cols, row, col, &area, &perimeter);
-      /* printf("ch: %c ar: %d pm: %d\n", maze[row][col], area, perimeter); */
-      sum += area * perimeter;
+      trace(maze, marker, rows, cols, row, col, &area, &perimeter, &sides);
+      printf("ch: %c ar: %d pm: %d sides: %d\n", maze[row][col], area,
+             perimeter, sides);
+      sum1 += area * perimeter;
+      sum2 += area * sides;
     }
   }
 
-  printf("sum is: %ld\n", sum);
+  printf("sum1 is: %ld\n", sum1);
+  printf("sum2 is: %ld\n", sum2);
 
   for (int i = 0; i < rows; i++) {
     free(maze[i]);
