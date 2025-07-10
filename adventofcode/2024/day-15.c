42abd69d84a5824c3bb98a6d6f7850f115a5b348
Shanavas M <shanavas.m2@gmail.com>
2025-07-10 22:28:09 +0530

    [aoc24] day-15 part-2

--- adventofcode/2024/day-15.c
+++ adventofcode/2024/day-15.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GRID_SIZE 64
+#define GRID_SIZE 128
 
 int move_box_right(char grid[][GRID_SIZE], int rows, int cols, int bx, int by) {
     for (int i = bx; i < cols; i++) {
@@ -120,6 +120,270 @@ int move(char grid[][GRID_SIZE], int rows, int cols, int *x, int *y, char sym) {
     return moved;
 }
 
+int move_box_right2(char grid[][GRID_SIZE], int rows, int cols, int bx,
+                    int by) {
+    for (int i = bx; i < cols; i++) {
+        if (grid[by][i] == '#') {
+            break;
+        } else if (grid[by][i] == '.') {
+            for (int j = i; j > bx; j--) {
+                grid[by][j] = grid[by][j - 1];
+            }
+            grid[by][bx] = '.';
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+int move_box_left2(char grid[][GRID_SIZE], int rows, int cols, int bx, int by) {
+    for (int i = bx; i > 0; i--) {
+        if (grid[by][i] == '#') {
+            break;
+        } else if (grid[by][i] == '.') {
+            for (int j = i; j < bx; j++) {
+                grid[by][j] = grid[by][j + 1];
+            }
+
+            grid[by][bx] = '.';
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int can_move_box_up(char grid[][GRID_SIZE], int rows, int cols, int bx,
+                    int by) {
+    int c1 = bx;
+    int c2;
+    if (grid[by][bx] == '[') {
+        c2 = bx + 1;
+    } else {
+        c2 = bx - 1;
+    }
+
+    if (grid[by - 1][c1] == '#' || grid[by - 1][c2] == '#') {
+        return 0;
+    }
+
+    if (grid[by - 1][c1] == '.' && grid[by - 1][c2] == '.') {
+        return 1;
+    }
+
+    if (grid[by - 1][c1] != '.' && grid[by - 1][c2] != '.') {
+        // above both c1 and c2 are objects
+        if (grid[by - 1][c1] == grid[by][c1]) {
+            // objects are rightly alighned
+            return can_move_box_up(grid, rows, cols, c1, by - 1);
+        } else {
+            return can_move_box_up(grid, rows, cols, c1, by - 1) &&
+                   can_move_box_up(grid, rows, cols, c2, by - 1);
+        }
+    }
+
+    // one of them is . and other is an object
+    if (grid[by - 1][c1] == '.') {
+        // above c2 is an object
+        return can_move_box_up(grid, rows, cols, c2, by - 1);
+    }
+
+    // above c1 is an object
+    return can_move_box_up(grid, rows, cols, c1, by - 1);
+}
+
+void do_move_box_up2(char grid[][GRID_SIZE], int rows, int cols, int bx,
+                     int by) {
+    int c1 = bx;
+    int c2;
+    if (grid[by][bx] == '[') {
+        c2 = bx + 1;
+    } else {
+        c2 = bx - 1;
+    }
+
+    if (grid[by - 1][c1] == '.' && grid[by - 1][c2] == '.') {
+        // do nothing
+    } else if (grid[by - 1][c1] == '.') {
+        // move c2
+        do_move_box_up2(grid, rows, cols, c2, by - 1);
+    } else if (grid[by - 1][c2] == '.') {
+        // move c1
+        do_move_box_up2(grid, rows, cols, c1, by - 1);
+    } else if (grid[by - 1][c2] == grid[by][c2]) {
+        // boxes aligned
+        do_move_box_up2(grid, rows, cols, c2, by - 1);
+    } else {
+        // move both boxes
+        do_move_box_up2(grid, rows, cols, c1, by - 1);
+        do_move_box_up2(grid, rows, cols, c2, by - 1);
+    }
+
+    grid[by - 1][c1] = grid[by][c1];
+    grid[by - 1][c2] = grid[by][c2];
+    grid[by][c1] = '.';
+    grid[by][c2] = '.';
+}
+
+int move_box_up2(char grid[][GRID_SIZE], int rows, int cols, int bx, int by) {
+    if (grid[by][bx] == '#') {
+        return 0;
+    }
+
+    // push a box
+
+    if (can_move_box_up(grid, rows, cols, bx, by)) {
+        do_move_box_up2(grid, rows, cols, bx, by);
+        return 1;
+    }
+
+    return 0;
+}
+
+int can_move_box_down(char grid[][GRID_SIZE], int rows, int cols, int bx,
+                      int by) {
+    int c1 = bx;
+    int c2;
+    if (grid[by][bx] == '[') {
+        c2 = bx + 1;
+    } else {
+        c2 = bx - 1;
+    }
+
+    if (grid[by + 1][c1] == '#' || grid[by + 1][c2] == '#') {
+        return 0;
+    }
+
+    if (grid[by + 1][c1] == '.' && grid[by + 1][c2] == '.') {
+        return 1;
+    }
+
+    if (grid[by + 1][c1] != '.' && grid[by + 1][c2] != '.') {
+        // above both c1 and c2 are objects
+        if (grid[by + 1][c1] == grid[by][c1]) {
+            // objects are rightly alighned
+            return can_move_box_down(grid, rows, cols, c1, by + 1);
+        } else {
+            return can_move_box_down(grid, rows, cols, c1, by + 1) &&
+                   can_move_box_down(grid, rows, cols, c2, by + 1);
+        }
+    }
+
+    // one of them is . and other is an object
+    if (grid[by + 1][c1] == '.') {
+        // below c2 is an object
+        return can_move_box_down(grid, rows, cols, c2, by + 1);
+    }
+
+    // below c1 is an object
+    return can_move_box_down(grid, rows, cols, c1, by + 1);
+}
+
+void do_move_box_down2(char grid[][GRID_SIZE], int rows, int cols, int bx,
+                       int by) {
+    int c1 = bx;
+    int c2;
+    if (grid[by][bx] == '[') {
+        c2 = bx + 1;
+    } else {
+        c2 = bx - 1;
+    }
+
+    if (grid[by + 1][c1] == '.' && grid[by + 1][c2] == '.') {
+        // do nothing
+    } else if (grid[by + 1][c1] == '.') {
+        // move c2
+        do_move_box_down2(grid, rows, cols, c2, by + 1);
+    } else if (grid[by + 1][c2] == '.') {
+        // move c1
+        do_move_box_down2(grid, rows, cols, c1, by + 1);
+    } else if (grid[by + 1][c2] == grid[by][c2]) {
+        // boxes aligned
+        do_move_box_down2(grid, rows, cols, c2, by + 1);
+    } else {
+        // move both boxes
+        do_move_box_down2(grid, rows, cols, c1, by + 1);
+        do_move_box_down2(grid, rows, cols, c2, by + 1);
+    }
+
+    grid[by + 1][c1] = grid[by][c1];
+    grid[by + 1][c2] = grid[by][c2];
+    grid[by][c1] = '.';
+    grid[by][c2] = '.';
+}
+
+int move_box_down2(char grid[][GRID_SIZE], int rows, int cols, int bx, int by) {
+    if (grid[by][bx] == '#') {
+        return 0;
+    }
+
+    // push a box
+    if (can_move_box_down(grid, rows, cols, bx, by)) {
+        do_move_box_down2(grid, rows, cols, bx, by);
+        return 1;
+    }
+
+    return 0;
+}
+
+int move2(char grid[][GRID_SIZE], int rows, int cols, int *x, int *y,
+          char sym) {
+
+    int rx = *x;
+    int ry = *y;
+
+    int moved = 0;
+
+    switch (sym) {
+    case '>':
+        // move right
+        if (grid[ry][rx + 1] == '.' ||
+            move_box_right2(grid, rows, cols, rx + 1, ry)) {
+            grid[ry][rx + 1] = '@';
+            grid[ry][rx] = '.';
+            moved = 1;
+            *x += 1;
+        }
+        break;
+    case '<':
+        // move left
+        if (grid[ry][rx - 1] == '.' ||
+            move_box_left2(grid, rows, cols, rx - 1, ry)) {
+            grid[ry][rx - 1] = '@';
+            grid[ry][rx] = '.';
+            moved = 1;
+            *x -= 1;
+        }
+
+        break;
+    case 'v':
+        // move down
+        if (grid[ry + 1][rx] == '.' ||
+            move_box_down2(grid, rows, cols, rx, ry + 1)) {
+            grid[ry + 1][rx] = '@';
+            grid[ry][rx] = '.';
+            moved = 1;
+            *y += 1;
+        }
+
+        break;
+    case '^':
+        // move up
+        if (grid[ry - 1][rx] == '.' ||
+            move_box_up2(grid, rows, cols, rx, ry - 1)) {
+            grid[ry - 1][rx] = '@';
+            grid[ry][rx] = '.';
+            moved = 1;
+            *y -= 1;
+        }
+        break;
+    default:
+        break;
+    }
+
+    return moved;
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 1) {
         exit(1);
@@ -133,12 +397,14 @@ int main(int argc, char *argv[]) {
     }
 
     char grid[GRID_SIZE][GRID_SIZE] = {0};
+    char grid2[GRID_SIZE][GRID_SIZE] = {0};
     char line[1024];
 
     // read maze
     int rows = 0;
     int cols = 0;
-    int rx, ry; // robot position
+    int rx, ry;   // robot position
+    int rx2, ry2; // robot position
 
     char *r;
     while (fgets(grid[rows], sizeof(grid[rows]), file)) {
@@ -147,24 +413,58 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-        if ((r = strchr(grid[rows], '@'))) {
-            ry = rows;
-            rx = r - grid[rows];
+        for (int i = 0; i < strlen(grid[rows]); i++) {
+            switch (grid[rows][i]) {
+            case '#':
+                grid2[rows][i * 2] = '#';
+                grid2[rows][i * 2 + 1] = '#';
+                break;
+            case 'O':
+                grid2[rows][i * 2] = '[';
+                grid2[rows][i * 2 + 1] = ']';
+                break;
+            case '.':
+                grid2[rows][i * 2] = '.';
+                grid2[rows][i * 2 + 1] = '.';
+                break;
+            case '@':
+                ry = rows;
+                rx = i;
+                ry2 = rows;
+                rx2 = i * 2;
+                grid2[rows][i * 2] = '@';
+                grid2[rows][i * 2 + 1] = '.';
+                break;
+            default:
+                break;
+            }
         }
 
         rows++;
     }
 
+    int rows2 = rows;
+    int cols2 = cols * 2;
+
     printf("r: %d, c: %d, rx: %d, ry: %d\n", rows, cols, rx, ry);
-    for (int i = 0; i < rows; i++) {
-        printf("%s", grid[i]);
+    printf("r2: %d, c2: %d, rx2: %d, ry2: %d\n", rows2, cols2, rx2, ry2);
+    for (int i = 0; i < rows2; i++) {
+        printf("%s\n", grid2[i]);
     }
 
     char ch;
     while (fgets(line, sizeof(line), file)) {
         for (int i = 0; i < strlen(line); i++) {
             ch = line[i];
-            (move(grid, rows, cols, &rx, &ry, ch));
+            move(grid, rows, cols, &rx, &ry, ch);
+            move2(grid2, rows2, cols2, &rx2, &ry2, ch);
+
+            /* printf("---move %c-----\n", ch); */
+            /* for (int k = 0; k < rows2; k++) { */
+            /*     printf("%s\n", grid2[k]); */
+            /* } */
+
+            /* getc(stdin); */
         }
     }
 
@@ -178,7 +478,18 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("sum of GPS: %ld", score);
+    long score2 = 0;
+    for (int i = 0; i < rows2; i++) {
+        printf("%s\n", grid2[i]);
+        for (int j = 0; j < cols2; j++) {
+            if (grid2[i][j] == '[') {
+                score2 += 100 * i + j;
+            }
+        }
+    }
+
+    printf("sum of GPS: %ld\n", score);
+    printf("sum of GPS2: %ld\n", score2);
 
     fclose(file);
 }
